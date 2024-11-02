ca67dbbbc73c10249e6b508a03243e2ef42e9e75
James Holman <jameswarringtonholman@gmail.com>
2024-11-02 15:52:15 +1100

    fix: format

--- src/common/utils.rs
+++ src/common/utils.rs
@@ -18,9 +18,7 @@ pub fn tokenstream_from_string(input: &str) -> Result<proc_macro2::TokenStream, 
 pub fn pretty_print_tokenstream(ts: proc_macro2::TokenStream) -> String {
     match parse2::<File>(ts.clone()) {
         Ok(file) => prettyplease::unparse(&file).to_string(),
-        Err(err) => format!(
-            "Failed to parse TokenStream: {err}. Stream was {ts}"
-        ),
+        Err(err) => format!("Failed to parse TokenStream: {err}. Stream was {ts}"),
     }
 }
 
